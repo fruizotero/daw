06f973caf1e27b61300e75ce6be42c1759baaf1b
fruizotero <frank.git96@gmail.com>
2023-11-01 18:00:58 +0100

    php/Tarea03.1

--- php/Tarea02.1/index.php
+++ php/Tarea02.1/index.php
@@ -1,12 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once "utils.php";
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarea02.1</title>
+</head>
 
-const RUTA =   "archivos";
-const CLAVE_INPUT = "archivos";
+<body>
 
-if (isset($_FILES[CLAVE_INPUT])) {
-    lista_informacion_ficheros(CLAVE_INPUT, RUTA);
-} else {
-    echo "<p>Error, el name de tu campo no es válido o no estás utilizando él método POST</p>";
-}
+    <form enctype="multipart/form-data" action="" method="post">
+
+        <label for="archivos">Subir archivos</label>
+        <br>
+        <input type="file" id="archivos" name="archivos[]" accept="image/jpeg, image/jpg" multiple required>
+        <br>
+        <button type="submit">Enviar</button>
+
+
+
+    </form>
+
+
+    <?php
+    require_once "utils.php";
+
+    const PATH =   "archivos";
+    const CLAVE_INPUT = "archivos";
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        if (isset($_FILES[CLAVE_INPUT])) {
+            lista_informacion_ficheros(CLAVE_INPUT);
+        } else {
+            echo "<p>Error, el 'name' de tu campo no es válido </p>";
+        }
+    } 
+    // else{
+    //     echo "<p>No estás utilizando él método POST</p>";
+    // }
+
+    ?>
+</body>
+
+</html>
--- php/Tarea02.1/utils.php
+++ php/Tarea02.1/utils.php
@@ -1,70 +1,59 @@
 <?php
 
-function lista_informacion_ficheros(string $clave_array, string $ruta): void
+const NAME = "name";
+const ERROR = "error";
+const SIZE = "size";
+const TMP_NAME = "tmp_name";
+const SUCCESS = "exito";
+
+function lista_informacion_ficheros(string $clave_files): void
 {
 
-    if (!file_exists($ruta))
-        mkdir($ruta, 0770, true);
+    //Si no existe la carpeta indicada en el "path", se crea una
+    if (!file_exists(PATH))
+        mkdir(PATH, 0770, true);
 
 
-    // echo "<pre>";
-    // print_r($array_ficheros);
-    // echo "</pre>";
-
-    $array_ficheros = $_FILES[$clave_array];
-    //Se obtienen las claves del array asociativo
+    $array_ficheros = $_FILES[$clave_files];
     $keys = array_keys($array_ficheros);
-
-
-    //Como todas las claves tienen un array  asociado con la misma cantidad de elementos que coincide con el número de archivos enviados, cualquiera sirve para obtener el número de archivos que se ha enviado
     $cantidad_archivos = sizeof($array_ficheros[$keys[0]]);
-    $array_ficheros_validos = [];
-    $array_exito = ["exito" => true, "name" => ""];
-    $array_error = ["exito" => false, "error" => ""];
 
-    //Cada indice de los elementos de los arrays asociados a las claves coinciden con la información de un archivo especifico, es decir, cada indice 0 hara referencia a un dato del primer archivo.
+    $array_ficheros_validos = [];
+    $array_exito = [SUCCESS => true, "datos" => ""];
+    $array_error = [SUCCESS => false, "datos" => ""];
+
     for ($i = 0; $i < $cantidad_archivos; $i++) {
+
         $numero_fichero = $i + 1;
         echo "<p>Fichero $numero_fichero </p>";
+        // Lista ordernada
+        echo "<ol>";
+        foreach ($keys as $key) {
+            $value = $array_ficheros[$key][$i];
 
+            echo "<li>$key: $value</li>";
+        }
+        echo "</ol>";
 
-        $error = $array_ficheros["error"][$i];
-        $tmp_name = $array_ficheros["tmp_name"][$i];
+        $error = $array_ficheros[ERROR][$i];
+        $tmp_name = $array_ficheros[TMP_NAME][$i];
 
 
         if (is_uploaded_file($tmp_name) && $error == UPLOAD_ERR_OK) {
 
-            echo "<ol>";
-            foreach ($keys as $key) {
-                $value = $array_ficheros[$key][$i];
-
-                echo "<li>$key: $value</li>";
-            }
-            echo "</ol>";
-
-            $archivo = $ruta . DIRECTORY_SEPARATOR .  basename($array_ficheros["name"][$i]);
+            $archivo = PATH . DIRECTORY_SEPARATOR .  basename($array_ficheros[NAME][$i]);
 
 
             if (move_uploaded_file($tmp_name, $archivo)) {
-                $array_exito["name"] = $array_ficheros["name"][$i];
+                $array_exito["datos"] = datos_array($array_ficheros, $i);
                 $array_ficheros_validos[] = $array_exito;
             } else {
-                $array_error["error"] = array(
-                    "name" => $array_ficheros["name"][$i],
-                    "error" => $array_ficheros["error"][$i],
-                    "size" => $array_ficheros["size"][$i]
-                );
-
+                $array_error["datos"] = datos_array($array_ficheros, $i);
                 $array_ficheros_validos[] = $array_error;
             }
         } else {
-            echo "<p>Fichero no válido</p>";
-            $array_error["error"] = array(
-                "name" => $array_ficheros["name"][$i],
-                "error" => $array_ficheros["error"][$i],
-                "size" => $array_ficheros["size"][$i]
-            );
 
+            $array_error["datos"] = datos_array($array_ficheros, $i);
             $array_ficheros_validos[] = $array_error;
         }
     }
@@ -72,15 +61,31 @@ function lista_informacion_ficheros(string $clave_array, string $ruta): void
 
     for ($k = 0; $k < sizeof($array_ficheros_validos); $k++) {
 
-        if ($array_ficheros_validos[$k]["exito"]) {
-            $name = $array_ficheros_validos[$k]["name"];
-            echo "<p>Se ha guardado con éxito el fichero: $name</p>";
-        } else {
-            $name = $array_ficheros_validos[$k]["error"]["name"];
-            $error = $array_ficheros_validos[$k]["error"]["error"];
-            $size = $array_ficheros_validos[$k]["error"]["size"];
+        if ($array_ficheros_validos[$k][SUCCESS]) {
 
-            echo "<p>No se ha podido guardar el fichero:<br>Nombre: $name<br>Código error: $error<br>Tamaño: $size</p>";
+            $name = $array_ficheros_validos[$k]["datos"][NAME];
+            $error = $array_ficheros_validos[$k]["datos"][ERROR];
+            $size = number_format($array_ficheros_validos[$k]["datos"][SIZE] / 1024, 2);
+
+            echo "<p>Se ha guardado con éxito el fichero: <br>Nombre: $name<br>Código error: $error<br>Tamaño: $size KB</p>";
+        } else {
+
+            $name = $array_ficheros_validos[$k]["datos"][NAME];
+            $error = $array_ficheros_validos[$k]["datos"][ERROR];
+            $size = number_format($array_ficheros_validos[$k]["datos"][SIZE] / 1024, 2);
+
+            echo "<p>No se ha podido guardar el fichero:<br>Nombre: $name<br>Código error: $error<br>Tamaño: $size KB</p>";
         }
     }
+}
+
+
+function datos_array($array, $index)
+{
+
+    return array(
+        NAME => $array[NAME][$index],
+        ERROR => $array[ERROR][$index],
+        SIZE => $array[SIZE][$index]
+    );
 }
