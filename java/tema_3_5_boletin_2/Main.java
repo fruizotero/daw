970d8dc4e522f97ab2558b6953f3536ea277720d
Frank Edgar Ruiz Otero <a21frankero@ateis.local>
2023-11-20 20:19:15 +0100

    numero omirp y armstrong

--- java/tema_3_5_boletin_2/Main.java
+++ java/tema_3_5_boletin_2/Main.java
@@ -65,64 +65,63 @@ public class Main {
 
     }
 
+    public static boolean numeroArmstrong(int numero) {
+
+        int sumaDigitosElevados = 0;
+
+        String[] numerosString = String.valueOf(numero).split("");
+        int potencia = numerosString.length;
+
+        for (String numeroString : numerosString) {
+            int digito = Integer.parseInt(numeroString);
+            sumaDigitosElevados += Math.pow(digito, potencia);
+        }
+
+        return sumaDigitosElevados == numero;
+    }
+
+    public static boolean esOmirp(int numero) {
+
+        return esPrimo(numero) && esPrimo(numeroInvertido(numero));
+
+    }
+
+    public static int numeroInvertido(int numero) {
+
+        String[] digitosNumeros = String.valueOf(numero).split("");
+
+        String numeroInvertidoString = "";
+        for (int i = digitosNumeros.length - 1; i >= 0; i--) {
+            numeroInvertidoString += digitosNumeros[i];
+        }
+
+        return Integer.parseInt(numeroInvertidoString);
+    }
+
+    public static boolean esPrimo(int numero) {
+
+        int contador = 0;
+
+        if (numero == 1) return false;
+
+        for (int i = 1; i <= numero; i++) {
+
+            if (numero % i == 0) contador++;
+        }
+        return contador == 2;
+
+    }
+
+
     public static void main(String[] args) {
         // mostrarMenorMayor(new int[] { -1, -2, 5, 4, -1, 2, 3, 10, 15 });
         // figuraTrianguloRectangulo(5);
         // figuraRombo(21);
 
-        String[][] crucigrama = new String[][] {
-                { "0 0", "0 1", "0 2", "0 3", "0 4" },
-                { "1 0", "1 1", "1 2", "1 3", "1 4" },
-                { "2 0", "2 1", "2 2", "2 3", "2 4" },
-                { "3 0", "3 1", "3 2", "3 3", "3 4" },
-                { "4 0", "4 1", "4 2", "4 3", "4 4" },
-        };
+//        System.out.println(numeroArmstrong(4210818));
 
-        // for (int i = crucigrama.length-1; i >= 0; i--) {
-        // // for (int i = 0; i < crucigrama.length; i++) {
-        // // int tmp = 0;
-        // System.out.print("i:" + i);
-        // System.out.println();
-        // int tmp_i = i;
-        // for (int j = 0; j <= i; j++) {
-
-        // if (j != 0)
-        // tmp_i--;
-        // System.out.printf("i: %s j:%s\n", tmp_i, j);
-        // }
-
-        // }
-
-        // int numRows = crucigrama.length;
-        // int numCols = crucigrama[0].length;
-
-        // for (int d = 0; d < numRows + numCols - 1; d++) {
-        // // Calcular las coordenadas iniciales de la diagonal
-        // int row = (d < numRows) ? d : numRows - 1;
-        // int col = (d < numRows) ? 0 : d - numRows + 1;
-
-        // // Imprimir elementos de la diagonal
-        // while (row >= 0 && col < numCols) {
-        // System.out.print(crucigrama[row][col] + " ");
-        // row--;
-        // col++;
-        // }
-        // System.out.println();
-        // }
-
-        // aa: for (int i = 1; i <= 3; i++) {
-        //     bb: for (int j = 1; j <= 3; j++) {
-        //         if (i == 2 && j == 2) {
-        //             // using break statement with label
-        //             break aa;
-        //         }
-        //         System.out.println(i + " " + j);
-        //     }
-        // }
-
-        Persona p= new Persona("frank");
-
-        p.imprimirDatos();
+//        System.out.println(esPrimo(1597));
+        System.out.println(esOmirp(13751));
 
     }
 }
